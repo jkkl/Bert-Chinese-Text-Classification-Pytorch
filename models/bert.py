befa3f356df6cfd609d98043f6a03a653f4a58a3
wangyuanzhuo <wangyuanzhuo@xiaoice.com>
2020-12-09 10:14:21 +0800

    run intention

--- models/bert.py
+++ models/bert.py
@@ -14,7 +14,7 @@ class Config(object):
         self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
         self.test_path = dataset + '/data/test.txt'                                  # 测试集
         self.class_list = [x.strip() for x in open(
-            dataset + '/data/class.txt').readlines()]                                # 类别名单
+            dataset + '/data/class.txt', encoding='utf-8').readlines()]
         self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
 
