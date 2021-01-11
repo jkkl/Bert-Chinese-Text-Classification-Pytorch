ee6c9c10df7ab6a0a2a43eb29c808b1cf52b8312
wangyuanzhuo <wangyuanzhuo@xiaoice.com>
2021-01-12 06:10:21 +0800

    run scl

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 import time
 import torch
 import numpy as np
-from train_eval import train, init_network
+from train_eval import train, init_network, test
 from importlib import import_module
 import argparse
 from utils import build_dataset, build_iterator, get_time_dif
@@ -15,7 +15,7 @@ args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    dataset = 'Intention136'  # 数据集
+    dataset = 'Intention2'  # 数据集
 
     model_name = args.model  # bert
     x = import_module('models.' + model_name)
@@ -37,3 +37,4 @@ if __name__ == '__main__':
     # train
     model = x.Model(config).to(config.device)
     train(config, model, train_iter, dev_iter, test_iter)
+    test(config, model, test_iter)
